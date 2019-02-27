a6c93a63502de05aeb8bd2f36e67db6e8eb0fbe4
sulabh2000 <bansalsulabh13@gmail.com>
2019-02-27 11:48:29 +0530

    init commit

---
+++ src/main/CircularList.java
@@ -0,0 +1,51 @@
+package main;
+
+public class CircularList {
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+
+        }
+        Node(int data, Node next)
+        {
+            this.data=data;
+            this.next=next;
+        }
+
+    }
+    int size=0;
+    Node head=null;
+    Node tail = null;
+
+
+    public static void main(String[] args) {
+        {
+            CircularList list = new CircularList();
+            System.out.println(list);
+        }
+    }
+    @Override
+    public String toString()
+    {
+        StringBuilder response=new StringBuilder();
+        response.append("[");
+        Node temp = this.head;
+        while(temp!=null)
+        {
+            response.append(temp.data);
+            if(temp.next!=null)
+            {
+                response.append(" ==> ");
+            }
+            temp=temp.next;
+        }
+        response.append("]");
+        return response.toString();
+    }
+
+}
